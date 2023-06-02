7ab0bcd4e1ea7ee27c69916ed9c34ff18cfc2bc8
Tirth Bhatt <tirth.b@simformsolutions.com>
2023-06-02 14:27:45 +0530

    Dashboard,boilerplate and static files added

---
+++ Splitgood/views.py
@@ -0,0 +1,48 @@
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout, authenticate, login
+from django.views.decorators.cache import cache_control, never_cache
+
+
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+@never_cache
+def home(request):
+    return render(request, "home.html")
+
+
+def registration(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        print(username)
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+
+        user = User.objects.create_user(username)
+        user.set_password(password)
+        user.email = email
+        user.save()
+        print("Registration successful")
+
+    return render(request, "home.html")
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            # return redirect('registration')
+            print("Error Occurred")
+        login(request, user)
+    return render(request, "dashboard.html")
+
+
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+@never_cache
+def logout_view(request):
+    logout(request)
+    print("Logout successful")
+    return render(request, "home.html")
---
+++ static/js/add_group.js
@@ -0,0 +1,56 @@
+var app = angular.module('demo', ['ngSanitize', 'ui.select']);
+
+/**
+ * AngularJS default filter with the following expression:
+ * "person in people | filter: {name: $select.search, age: $select.search}"
+ * performs an AND between 'name: $select.search' and 'age: $select.search'.
+ * We want to perform an OR.
+ */
+app.filter('propsFilter', function() {
+  return function(items, props) {
+    var out = [];
+
+    if (angular.isArray(items)) {
+      var keys = Object.keys(props);
+
+      items.forEach(function(item) {
+        var itemMatches = false;
+
+        for (var i = 0; i < keys.length; i++) {
+          var prop = keys[i];
+          var text = props[prop].toLowerCase();
+          if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
+            itemMatches = true;
+            break;
+          }
+        }
+
+        if (itemMatches) {
+          out.push(item);
+        }
+      });
+    } else {
+      // Let the output be the input untouched
+      out = items;
+    }
+
+    return out;
+  };
+});
+
+app.controller('DemoCtrl', function ($scope, $http, $timeout, $interval) {
+  var vm = this;
+
+  vm.people = [
+    { name: 'Adam',      email: 'adam@email.com',      age: 12, country: 'United States' },
+    { name: 'Wladimir',  email: 'wladimir@email.com',  age: 30, country: 'Ecuador' },
+    { name: 'Samantha',  email: 'samantha@email.com',  age: 30, country: 'United States' }
+  ];
+
+  vm.multipleDemo = {};
+  vm.multipleDemo.selectedPeople = [vm.people[5], vm.people[4]];
+  vm.multipleDemo.selectedPeople2 = vm.multipleDemo.selectedPeople;
+  vm.multipleDemo.selectedPeopleWithGroupBy = [vm.people[8], vm.people[6]];
+  vm.multipleDemo.selectedPeopleSimple = ['samantha@email.com','wladimir@email.com'];
+
+});
